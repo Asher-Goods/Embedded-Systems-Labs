bafe1a2bc8992f1ed0f4a73d2bdc2de25d4cd33c
Asher_Goods <asheragoodwin@gmail.com>
2023-09-29 12:54:51 -0400

    added matrix multiplication function

--- task_1.c
+++ task_1.c
@@ -8,31 +8,46 @@ float A[N][N];
 float B[N][N];
 float C[N][N];
 
+// Function to multiply two matrices A and B of size N
+void multiplyMatrices(float A[][N], float B[][N], float C[][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            C[i][j] = 0; // Initialize the result matrix element to 0
+            for (int k = 0; k < N; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
 int main()
 {
 
     /* Randomly initialize the matrices */
     srand((unsigned)time(NULL));
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            A[i][j] = rand()/(float)RAND_MAX;
-            B[i][j] = rand()/(float)RAND_MAX;
+            A[i][j] = rand() / (float)RAND_MAX;
+            B[i][j] = rand() / (float)RAND_MAX;
         }
     }
 
     /*timer setup*/
     clock_t start_time = clock();
 
-    /* Code for matrix multiplication    C <- A x B*/ 
-
+    /* Code for matrix multiplication    C <- A x B*/
+    multiplyMatrices(A, B, C);
 
     clock_t end_time = clock();
     float elapse = (float)(end_time - start_time) / CLOCKS_PER_SEC;
+    
     printf("elapse: %.4f seconds\n", elapse);
 
     return 0;
-    // test
 }
-
